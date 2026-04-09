e57dd510ddcf85a6e959ef62190b4056800bbb26
201920732 이의훈 <uihoonpop@gmail.com>
2026-04-09 23:28:28 +0900

    Lab 5

---
+++ code/1-3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void print_reverse(int *arr, int size)
+{
+    int *p;
+
+    p = arr + size - 1;
+
+    for (int i = 0; i < size; i++) {
+        if (i == size - 1)
+            printf("%d", *p);
+        else
+            printf("%d ", *p);
+        p--;
+    }
+}
+
+int main()
+{
+    int nums[] = {1, 2, 3, 4, 5};
+
+    print_reverse(nums, 5);
+    printf("\n");
+
+    return 0;
+}
